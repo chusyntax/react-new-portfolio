05c13207bbc8b1c44cc3b51cef72c4695a4dbc44
chu-young <ttheko101@gmail.com>
2021-08-20 23:31:44 +0200

    added extra modal

--- src/Projects.js
+++ src/Projects.js
@@ -1,17 +1,38 @@
-import React, { useState } from 'react';
 import { Modal } from 'react-responsive-modal';
+import React, { useState } from 'react';
+
 
 const Projects = () => {
-  const [open, setOpen] = useState(false);
+   
+
+ const [open, setOpen] = useState(false);
+const [open2, setOpen2] = useState(false);
+
 
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
 
+  const onOpenModal2 = () => setOpen(true);
+  const onCloseModal2 = () => setOpen(false);
+
   return (
-      <>
+    <div className='cards'>
+
     <div>
       <button onClick={onOpenModal}>Open modal</button>
       <Modal open={open} onClose={onCloseModal} center>
+        <h2>Simple centered modal</h2>
+        <p>
+          Lorem ipsum dolor sit amet,   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
+          hendrerit risus, sed porttitor quam.
+        </p>
+      </Modal>
+    </div>
+
+     <div>
+      <button onClick={onOpenModal2}>Open modal</button>
+      <Modal open={open2} onClose={onCloseModal2} center>
         <h2>Simple centered modal</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
@@ -20,41 +41,10 @@ const Projects = () => {
         </p>
       </Modal>
     </div>
-    <div>
-      <button onClick={onOpenModal}>Open modal</button>
-      <Modal open={open} onClose={onCloseModal} center>
-        <h2>Simple centered modal</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-          hendrerit risus, sed porttitor quam.
-        </p>
-      </Modal>
+
     </div>
-    <div>
-      <button onClick={onOpenModal}>Open modal</button>
-      <Modal open={open} onClose={onCloseModal} center>
-        <h2>Simple centered modal</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-          hendrerit risus, sed porttitor quam.
-        </p>
-      </Modal>
-    </div>
-    <div>
-      <button onClick={onOpenModal}>Open modal</button>
-      <Modal open={open} onClose={onCloseModal} center>
-        <h2>Simple centered modal</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-          hendrerit risus, sed porttitor quam.
-        </p>
-      </Modal>
-    </div>
-    </>
+    
   );
-};
+ };
 
 export default Projects
