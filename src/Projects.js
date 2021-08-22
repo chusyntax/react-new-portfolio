e0ef704ed332713d8e0ead779ba5cedcea10c67b
chu-young <ttheko101@gmail.com>
2021-08-22 09:58:07 +0200

    added first group of cards which are functional

--- src/Projects.js
+++ src/Projects.js
@@ -3,25 +3,32 @@ import React, { useState } from 'react';
 
 
 const Projects = () => {
-   
-
- const [open, setOpen] = useState(false);
+         
+const [open, setOpen] = useState(false);
 const [open2, setOpen2] = useState(false);
+const [open3, setOpen3] = useState(false);
+const [open4, setOpen4] = useState(false);
 
-
+  
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
 
-  const onOpenModal2 = () => setOpen(true);
-  const onCloseModal2 = () => setOpen(false);
+  const onOpenModal2 = () => setOpen2(true);
+  const onCloseModal2 = () => setOpen2(false);
+
+  const onOpenModal3 = () => setOpen3(true);
+  const onCloseModal3 = () => setOpen3(false);
+
+  const onOpenModal4 = () => setOpen4(true);
+  const onCloseModal4 = () => setOpen4(false);
 
   return (
-    <div className='cards'>
+    <div className='modals'>
 
     <div>
-      <button onClick={onOpenModal}>Open modal</button>
+      <button onClick={onOpenModal}>Open 1</button>
       <Modal open={open} onClose={onCloseModal} center>
-        <h2>Simple centered modal</h2>
+        <h2>Simple centered modal 1</h2>
         <p>
           Lorem ipsum dolor sit amet,   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
           pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
@@ -31,9 +38,39 @@ const [open2, setOpen2] = useState(false);
     </div>
 
      <div>
-      <button onClick={onOpenModal2}>Open modal</button>
+      <button onClick={onOpenModal2}>Open 2</button>
       <Modal open={open2} onClose={onCloseModal2} center>
-        <h2>Simple centered modal</h2>
+        <h2>Simple centered modal 2</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
+          hendrerit risus, sed porttitor quam.
+        </p>
+      </Modal>
+    </div>
+
+     <div>
+      <button onClick={onOpenModal3}>Open 3</button>
+      <Modal open={open3} onClose={onCloseModal3} center>
+        <h2>Simple centered modal 3</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
+          hendrerit risus, sed porttitor quam.
+        </p>
+      </Modal>
+    </div>
+
+
+    <div>
+      <button onClick={onOpenModal4}>Open 4</button>
+      <Modal open={open4} onClose={onCloseModal4} center>
+        <h2>Simple centered modal 4</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
+          hendrerit risus, sed porttitor quam.
+        </p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
           pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
