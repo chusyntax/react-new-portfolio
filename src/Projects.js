3bfb658c8ef19ec9ec7cc1b017468cc75d757758
chu-young <ttheko101@gmail.com>
2021-08-23 14:42:00 +0200

    fixed all modals

--- src/Projects.js
+++ src/Projects.js
@@ -6,14 +6,18 @@ import styles from './Projects.module.css';
 
 const Projects = () => {
          
-const [open, setOpen] = useState(false);
+const [open1, setOpen1] = useState(false);
 const [open2, setOpen2] = useState(false);
 const [open3, setOpen3] = useState(false);
 const [open4, setOpen4] = useState(false);
+const [open5, setOpen5] = useState(false);
+const [open6, setOpen6] = useState(false);
+const [open7, setOpen7] = useState(false);
+const [open8, setOpen8] = useState(false);
 
   
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
+  const onOpenModal1 = () => setOpen1(true);
+  const onCloseModal1 = () => setOpen1(false);
 
   const onOpenModal2 = () => setOpen2(true);
   const onCloseModal2 = () => setOpen2(false);
@@ -23,6 +27,18 @@ const [open4, setOpen4] = useState(false);
 
   const onOpenModal4 = () => setOpen4(true);
   const onCloseModal4 = () => setOpen4(false);
+
+  const onOpenModal5 = () => setOpen5(true);
+  const onCloseModal5 = () => setOpen5(false);
+
+  const onOpenModal6 = () => setOpen6(true);
+  const onCloseModal6 = () => setOpen6(false);
+
+  const onOpenModal7 = () => setOpen7(true);
+  const onCloseModal7 = () => setOpen7(false);
+
+  const onOpenModal8 = () => setOpen8(true);
+  const onCloseModal8 = () => setOpen8(false);
 
   return (
 <>
@@ -35,10 +51,10 @@ const [open4, setOpen4] = useState(false);
    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
    gitHub="https://github.com/chusyntax"
    liveSite="https://github.com/chusyntax"
-   onClick={onOpenModal}
+   onClick={onOpenModal1}
    >
   </Card>
-      <Modal open={open} onClose={onCloseModal} center>
+      <Modal open={open1} onClose={onCloseModal1} center>
         <h2>Simple centered modal 1</h2>
         <p>
           Lorem ipsum dolor sit amet,   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
@@ -149,11 +165,11 @@ const [open4, setOpen4] = useState(false);
 description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
 gitHub="https://github.com/chusyntax"
 liveSite="https://github.com/chusyntax"
-onClick={onOpenModal}
+onClick={onOpenModal5}
 >
 </Card>
-  <Modal open={open} onClose={onCloseModal} center>
-    <h2>Simple centered modal 1</h2>
+  <Modal open={open5} onClose={onCloseModal5} center>
+    <h2>Simple centered modal 5</h2>
     <p>
       Lorem ipsum dolor sit amet,   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
       pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
@@ -167,11 +183,11 @@ onClick={onOpenModal}
 description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
 gitHub="https://github.com/chusyntax"
 liveSite="https://github.com/chusyntax"
-onClick={onOpenModal2}
+onClick={onOpenModal6}
 >
 </Card>
-  <Modal open={open2} onClose={onCloseModal2} center>
-    <h2>Simple centered modal 2</h2>
+  <Modal open={open6} onClose={onCloseModal6} center>
+    <h2>Simple centered modal 6</h2>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
       pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
@@ -185,11 +201,11 @@ onClick={onOpenModal2}
 description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
 gitHub="https://github.com/chusyntax"
 liveSite="https://github.com/chusyntax"
-onClick={onOpenModal3}
+onClick={onOpenModal7}
 >
 </Card>
-  <Modal open={open3} onClose={onCloseModal3} center>
-    <h2>Simple centered modal 3</h2>
+  <Modal open={open7} onClose={onCloseModal7} center>
+    <h2>Simple centered modal 7</h2>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
       pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
@@ -204,11 +220,11 @@ onClick={onOpenModal3}
 description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
 gitHub="https://github.com/chusyntax"
 liveSite="https://github.com/chusyntax"
-onClick={onOpenModal4}
+onClick={onOpenModal8}
 >
 </Card>
-  <Modal open={open4} onClose={onCloseModal4} center>
-    <h2>Simple centered modal 4</h2>
+  <Modal open={open8} onClose={onCloseModal8} center>
+    <h2>Simple centered modal 8</h2>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
       pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
