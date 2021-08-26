43578788129d900dc49160ae06545a083be9bd3a
chu-young <ttheko101@gmail.com>
2021-08-27 00:41:40 +0200

    added first five card images to Images file

---
+++ src/assets/Images.js
@@ -0,0 +1,18 @@
+const Images = {
+card1 : require('./card 1.png').default,
+card1Mobile : require('./card 1 mobile.png').default,
+
+card2 : require('./card 2.png').default,
+card2Mobile : require('./card 2 mobile.png').default,
+
+card3 : require('./card 3.png').default,
+card3Mobile : require('./card 3 mobile.png').default,
+
+card4 : require('./card 4.png').default,
+card4Mobile : require('./card 4 mobile.png').default,
+
+card5 : require('./card 5.png').default,
+card5Mobile : require('./card 5 mobile.png').default,
+}
+
+export default Images
